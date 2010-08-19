260d93baa6d3670e1ec2f419aa694dd440300281
Matt Wynne <matt@mattwynne.net>
2010-08-19 16:16:37 +0100

    Follow the cukes to get back to a feature that's failing for the right reasons

--- lib/practice.rb
+++ lib/practice.rb
@@ -2,6 +2,22 @@ class Visit < Struct.new(:owner, :pet, :operations)
 end
 
 class Account
+  def self.for(owner)
+    @accounts ||= {}
+    @accounts[owner] = new(owner)
+  end
+  
+  def initialize(owner)
+    @owner = owner
+  end
+  
+  def charge!(operation, price)
+    
+  end
+  
+  def produce_receipt(payment_type)
+    
+  end
 end
 
 class Practice
