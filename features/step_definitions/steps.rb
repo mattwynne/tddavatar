8e713d668f674b8d9a9e27ac1fdebf0c4bf3463f
Matt Wynne <matt@mattwynne.net>
2010-08-19 15:07:18 +0100

    Add stub (pending) step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,25 @@
+Given /^the following operations are available:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^there is an owner Dave Atkins, let's call him "([^"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^Dave brings his pet named Fluffy into the clinic for the following operations:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the veterinarian charges him for the visit$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^Dave pays cash$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^Dave is given a receipt which looks like this:$/ do |string|
+  pending # express the regexp above with the code you wish you had
+end
