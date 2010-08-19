e71b4a3fdbf094a28d8bd411d18e774243f95b2a
Matt Wynne <matt@mattwynne.net>
2010-08-19 16:18:50 +0100

    Add failing spec to drive out Account#produce_receipt method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Account do
-  
+  context "when a couple of operations have been performed" do
+    let(:owner) { "Dave" }
+    subject { Account.new(owner) }
+    
+    before(:each) do
+      subject.charge! "foo", 15
+      subject.charge! "bar", 20
+    end
+    
+    let(:receipt) { subject.produce_receipt("cash") }
+    
+    it "shows those operations in the output" do
+      receipt.should =~ /foo/
+      receipt.should =~ /bar/
+      receipt.should =~ /15/
+      receipt.should =~ /20/
+    end
+  end
 end
