398ccf5671204cddc2da1be3d6edc5b6b78c2a24
Matt Wynne <matt@mattwynne.net>
2010-08-19 16:13:54 +0100

    Add failing spec for Practice#receive_payment_from

--- spec/practice_spec.rb
+++ spec/practice_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe Practice do
+  let(:owner)   { "Dave" }
+  let(:account) { mock(Account) }
+  before(:each) do
+    Account.stub(:for).with(owner).and_return(account)
+  end
+
   describe "#charge_for_visit" do
-    let(:account) { mock(Account) }
-    let(:owner)   { "Dave" }
     let(:visit)   { Visit.new(owner, "Fluffy", ["injections", "check-up"]) }
 
-    before(:each) do
-      Account.stub(:for).with(owner).and_return(account)
-    end
     
     it "should charge the owner's account with the cost of each service" do
       subject.prices["injections"] = 10
@@ -17,6 +18,15 @@ describe Practice do
       account.should_receive(:charge!).with("injections", 10)
       account.should_receive(:charge!).with("check-up", 20)
       subject.charge_for_visit(visit)
+    end
+  end
+  
+  describe "#receive_payment_from" do
+    let(:receipt) { mock("Receipt") }
+    
+    it "asks the account to produce a receipt" do
+      account.should_receive(:produce_receipt).with("cash").and_return(receipt)
+      subject.receive_payment_from(owner, "cash").should == receipt
     end
   end
 end
