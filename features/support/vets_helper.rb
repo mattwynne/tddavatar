a7d31a957ee8eb5486a4f2d87d61e375ed619e3b
Matt Wynne <matt@mattwynne.net>
2010-08-19 15:56:29 +0100

    Implement our custom step defs DSL against an imaginary Practice class

--- features/support/vets_helper.rb
+++ features/support/vets_helper.rb
@@ -1,20 +1,38 @@
 module VetsHelper
   def register_operation_price(operation, price)
+    practice.prices[operation] = price
   end
   
   def remember_owner(name, nickname)
+    owners[nickname] = practice.create_owner(name)
   end
   
   def create_visit(owner_nickname, pet_name, operations)
+    owner = owners[owner_nickname]
+    @visit = practice.create_visit(owner, pet_name, operations)
   end
   
   def charge_for_visit
+    practice.charge_for_visit(@visit)
   end
   
   def pay_with(payment_type, nickname)
+    owner = owners[nickname]
+    @receipt = practice.receive_payment_from(owner, payment_type)
   end
   
   def receipt
+    @receipt.to_s
+  end
+  
+  private
+  
+  def practice
+    @practice ||= Practice.new
+  end
+  
+  def owners
+    @owners ||= {}
   end
 end
 
